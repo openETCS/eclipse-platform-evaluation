99ab3a4f6fd044bad94aa8216a5483317f1195a9
eneufeld <neufeld.eugen@googlemail.com>
2013-01-11 10:12:49 +0100

    added example code

--- org.openetcs.es3f/src/org/openetcs/es3f/ExportUtil.java
+++ org.openetcs.es3f/src/org/openetcs/es3f/ExportUtil.java
@@ -1,8 +1,13 @@
 package org.openetcs.es3f;
 
 import java.io.File;
+import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecp.core.ECPProject;
+import org.openetcs.model.ertmsformalspecs.Dictionary;
+import org.openetcs.model.ertmsformalspecs.ModelPackage;
+import org.openetcs.model.ertmsformalspecs.Namespace;
 
 /**
  * This is the Util class for exporting a {@link ECPProject} containing a model to a es3f file.
@@ -13,6 +18,15 @@ import org.eclipse.emf.ecp.core.ECPProject;
 public class ExportUtil {
 
 	public static void exportModel(ECPProject projectToExport, File fileToExport){
-		
+		List<Object> elements=projectToExport.getElements();
+		for(Object object:elements){
+			EObject eObject=(EObject)object;
+			if(ModelPackage.eINSTANCE.getDictionary().equals(eObject.eClass())){
+				Dictionary dictonary=(Dictionary)eObject;
+				for(Namespace namespace:dictonary.getNamespaces()){
+					
+				}
+			}
+		}
 	}
 }
--- org.openetcs.es3f/src/org/openetcs/es3f/ImportUtil.java
+++ org.openetcs.es3f/src/org/openetcs/es3f/ImportUtil.java
@@ -3,6 +3,10 @@ package org.openetcs.es3f;
 import java.io.File;
 
 import org.eclipse.emf.ecp.core.ECPProject;
+import org.openetcs.model.ertmsformalspecs.Dictionary;
+import org.openetcs.model.ertmsformalspecs.ModelFactory;
+import org.openetcs.model.ertmsformalspecs.ModelPackage;
+import org.openetcs.model.ertmsformalspecs.Namespace;
 
 /**
  * This is the Util class for importing a es3f file containing a model into an {@link ECPProject}.
@@ -13,6 +17,10 @@ import org.eclipse.emf.ecp.core.ECPProject;
 public class ImportUtil {
 
 	public static void importModel(File fileToImport, ECPProject importProject){
+		Dictionary dictionary=ModelFactory.eINSTANCE.createDictionary();
+		Namespace namespace=ModelFactory.eINSTANCE.createNamespace();
+		dictionary.getNamespaces().add(namespace);
 		
+		importProject.getElements().add(dictionary);
 	}
 }
