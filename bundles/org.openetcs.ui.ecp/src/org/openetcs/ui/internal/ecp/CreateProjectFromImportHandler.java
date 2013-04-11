11619c2352ee99faa295acabf5ab837f76da250a
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-04-11 10:50:58 +0200

    fixed compile errors

--- bundles/org.openetcs.ui.ecp/src/org/openetcs/ui/internal/ecp/CreateProjectFromImportHandler.java
+++ bundles/org.openetcs.ui.ecp/src/org/openetcs/ui/internal/ecp/CreateProjectFromImportHandler.java
@@ -8,7 +8,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecp.core.ECPProject;
-import org.eclipse.emf.ecp.ui.util.HandlerHelper;
+import org.eclipse.emf.ecp.internal.ui.util.ECPHandlerHelper;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
@@ -23,19 +23,19 @@ public class CreateProjectFromImportHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		FileDialog fd=new  FileDialog(HandlerUtil.getActiveShell(event), SWT.OPEN);
-		fd.setFilterExtensions(new String[]{"*.efs"});
-		final String pathToImport=fd.open();
-		if(pathToImport==null)
+
+		FileDialog fd = new FileDialog(HandlerUtil.getActiveShell(event), SWT.OPEN);
+		fd.setFilterExtensions(new String[] { "*.efs" });
+		final String pathToImport = fd.open();
+		if (pathToImport == null)
 			return null;
-		final ECPProject project=HandlerHelper.createProject(HandlerUtil.getActiveShell(event));
+		final ECPProject project = ECPHandlerHelper.createProject(HandlerUtil.getActiveShell(event));
 		IProgressService progressService = PlatformUI.getWorkbench().getProgressService();
 		try {
 			progressService.run(true, false, new IRunnableWithProgress() {
 				@Override
 				public void run(IProgressMonitor monitor) throws InvocationTargetException,
-						InterruptedException {
+					InterruptedException {
 					ImportUtil.importModel(new File(pathToImport), project, monitor);
 				}
 			});
@@ -46,11 +46,11 @@ public class CreateProjectFromImportHandler extends AbstractHandler {
 		}
 		return null;
 	}
-	
+
 	public void showImportErrorDialog(String message) {
-		MessageDialog.openError(Display.getCurrent().getActiveShell(), 
-				"Import error", 
-				"An error occurred during import: " + message);
+		MessageDialog.openError(Display.getCurrent().getActiveShell(),
+			"Import error",
+			"An error occurred during import: " + message);
 	}
 
 }
